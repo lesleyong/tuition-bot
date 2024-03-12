05de54c34512b28fab9aa384a972ad47cde3dcf8
lesley <lesleyongweibin@gmail.com>
2024-03-12 23:18:43 +0800

    first commit

---
+++ src/main/java/com/prac/tuitionbot/responsehandler/UserState.java
@@ -0,0 +1,5 @@
+package com.prac.tuitionbot.responsehandler;
+
+public enum UserState {
+    AWAITING_NAME, FOOD_DRINK_SELECTION, PIZZA_TOPPINGS, AWAITING_CONFIRMATION
+}
---
+++ src/main/java/com/prac/tuitionbot/util/Constants.java
@@ -0,0 +1,11 @@
+package com.prac.tuitionbot.util;
+
+public class Constants {
+
+    private Constants() {};
+
+    public static final String START_DESCRIPTION = "Starts the bot";
+
+    public static final String CHAT_STATES = "chatStates";
+    public static final String START_TEXT = "Welcome! I am your dedicated Tuition Butler. What is your unique tuition id?";
+}
